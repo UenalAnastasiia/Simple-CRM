3ee448af523ad5b5839d684f487f3d226420281e
Anastasiia Ünal <velyka.nastya@icloud.com>
2022-11-13 21:06:27 +0100

    paginator, filter, mat-table

--- src/app/user-data/user/user.component.ts
+++ src/app/user-data/user/user.component.ts
@@ -7,6 +7,7 @@ import { User } from 'src/models/user.class';
 import { DialogAddUserComponent } from '../dialog-add-user/dialog-add-user.component';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
 
 
 @Component({
@@ -17,6 +18,7 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class UserComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
+  @ViewChild(MatSort) sort: MatSort;
 
   user = new User();
   allUsers$: Observable<any>;
@@ -31,7 +33,6 @@ export class UserComponent implements OnInit {
 
   constructor(public dialog: MatDialog, private firestore: Firestore) { }
 
-
   ngOnInit(): void {
     const userCollection = collection(this.firestore, 'users');
     this.allUsers$ = collectionData(userCollection, { idField: "userID" });
@@ -41,6 +42,16 @@ export class UserComponent implements OnInit {
       this.dataSource = new MatTableDataSource(this.allUsers);
       this.dataSource.paginator = this.paginator;
     });
+  }
+
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 
 
