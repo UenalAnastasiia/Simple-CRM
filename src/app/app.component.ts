1b6014c72ac0f9e9fef00a58550fa488c8156fa8
Anastasiia Ünal <velyka.nastya@icloud.com>
2022-11-14 15:10:17 +0100

    sidenav responsive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
 import { Firestore, getDocs } from '@angular/fire/firestore';
+import { MatSidenav } from '@angular/material/sidenav';
 import { Router } from '@angular/router';
 import { collection } from '@firebase/firestore';
+import { BehaviorSubject, Observable } from 'rxjs';
 
 
 @Component({
@@ -10,11 +12,19 @@ import { collection } from '@firebase/firestore';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  @ViewChild('sidenav') matSidenav: MatSidenav;
   userLength: number;
   taskLength: number;
   contactLength = 0;
   hidden = false;
   homePage: boolean = true;
+
+  showToggle: string;
+  mode: any;
+  openSidenav: boolean;
+  private screenWidth$ = new BehaviorSubject<number>
+    (window.innerWidth);
+
 
   constructor(private firestore: Firestore, public router: Router) { }
 
@@ -23,9 +33,36 @@ export class AppComponent implements OnInit {
     this.checkURL();
     this.checkUserLength();
     this.checkTaskLength();
+    this.checkWidth();
     if (this.contactLength == 0) {
       this.hidden = !this.hidden;
     }
+  }
+
+
+  checkWidth() {
+    this.getScreenWidth().subscribe(width => {
+      if (width < 1050) {
+        this.showToggle = 'show';
+        this.mode = 'over';
+        this.openSidenav = false;
+      } else if (width > 1050) {
+        this.showToggle = 'hide';
+        this.mode = 'side';
+        this.openSidenav = true;
+      }
+    });
+  }
+
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.screenWidth$.next(event.target.innerWidth);
+  }
+
+
+  getScreenWidth(): Observable<number> {
+    return this.screenWidth$.asObservable();
   }
 
 
